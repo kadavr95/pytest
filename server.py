6d138c5a9d1f807389900c58a0c4df722db0fac0
kadavr95 <kadavr95@gmail.com>
2017-12-23 01:20:34 +0300

    -

--- server.py
+++ server.py
@@ -20,13 +20,3 @@ while True:
     client_socket.sendall(request)
 
     client_socket.close()
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    inputtext = input("")
-    client_socket.connect(("localhost", 8080))
-    client_socket.sendall(bytes(inputtext, encoding='utf-8'))
-
-    response = client_socket.recv(1024)
-
-    print(response.decode("UTF-8"))
-    client_socket.close()
